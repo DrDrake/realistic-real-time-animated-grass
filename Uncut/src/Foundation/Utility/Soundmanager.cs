232f204a350a5e197940794caa36c0393d340c6d
kaschubert <kaschubert@kaschubert-PC.fbihome.de>
2011-10-25 16:08:56 +0200

    merged projects

---
+++ Uncut/src/Foundation/Utility/Soundmanager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+using SlimDX;
+using SlimDX.XAudio2;
+using SlimDX.Multimedia;
+
+
+namespace Uncut.Utility
+{
+    class Soundmanager
+    {
+        XAudio2             m_device;
+        MasteringVoice      m_masteringVoice;
+        Thread              m_playThread;
+
+        public Soundmanager()
+        {
+            m_device = new XAudio2();
+            m_masteringVoice = new MasteringVoice(m_device);
+            //m_playThread = new Thread(this.playSingle);
+        }
+
+        ~Soundmanager()
+        {
+            m_masteringVoice.Dispose();
+            m_device.Dispose();
+        }
+
+        public void playSingle(string fileName)
+        {
+            //WaveStream stream = new WaveStream(fileName);
+            var s = System.IO.File.OpenRead(fileName);
+            WaveStream stream = new WaveStream(s);
+           
+            s.Close();
+
+            AudioBuffer buffer = new AudioBuffer();
+            buffer.AudioData = stream;
+            buffer.AudioBytes = (int) stream.Length;
+            buffer.Flags = BufferFlags.EndOfStream;
+
+            SourceVoice sourceVoice = new SourceVoice(m_device, stream.Format);
+            sourceVoice.SubmitSourceBuffer(buffer);
+            sourceVoice.Start();
+
+            while (sourceVoice.State.BuffersQueued > 0)
+            {
+                Thread.Sleep(10);
+            }
+
+            // cleanup the voice
+            buffer.Dispose();
+            sourceVoice.Dispose();
+            stream.Dispose();
+        }
+
+        public void playLoop(string filename)
+        {
+
+        }
+
+    }
+}
---
+++ Uncut/src/TestViewController.cs
@@ -0,0 +1,285 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+using SlimDX;
+using SlimDX.Direct3D10;
+using SlimDX.D3DCompiler;
+using SlimDX.DXGI;
+using SlimDX.DirectInput;
+
+using Uncut.Rendering;
+using Uncut.Rendering.UI;
+using Uncut.Utility;
+using Uncut.UI.Binding;
+using Uncut.UI;
+
+
+namespace Uncut
+{
+    /// <summary>
+    /// Demonstrates how to render a simple colored triangle with Direct3D10.
+    /// </summary>
+    class TestViewController : SlimSceneController
+    {
+        #region Implementation Detail
+
+        private RenderTargetView renderTargetView;
+        private DepthStencilState depthStencilState;
+        private DepthStencilView depthStencilView;
+        private Camera m_camera;
+        private InputController m_input;
+        private Soundmanager m_soundManager;
+        private Clock clock;
+        private readonly Bindable<float> output = new Bindable<float>();
+        private SimpleCube cube;
+        private SimplePlane plane;
+        private SimpleGrass straw;
+        private float[,] strawSize;
+
+        private Matrix m_proj;
+        private Matrix m_view;
+
+        #endregion
+
+        /// <summary>
+        /// In a derived class, implements logic to initialize the sample.
+        /// </summary>
+        protected override void OnInitialize()
+        {
+            var hudText = new Element();
+            hudText.SetBinding("Label", output);
+            UserInterface.Container.Add(hudText);
+
+            m_camera = new Camera(
+                new Vector3(0, 0, 100), // position
+                new Vector3(0, 0, 0), // lookat
+                Vector3.UnitZ, // direction
+                Vector3.UnitY, // up
+                0.03f, // moveSpeedMouse
+                1f, // moveSpeedKeys
+                1.0f, // near
+                1000.0f, // far
+                45.0f, // fov
+                WindowWidth / WindowHeight //aspect ratio
+            );
+            m_soundManager = new Soundmanager();
+            m_input = new InputController(m_form);
+
+            clock = new Clock();
+            clock.Start();
+
+            strawSize = new float[100,100];
+            System.Random r = new System.Random();
+
+            for (int col = 0; col < 100; ++col)
+            {
+                for (int row = 0; row < 100; ++row)
+                {
+                    strawSize.SetValue((float)r.NextDouble() * 0.1f, col, row);
+                }
+            }
+
+            DeviceSettings10 settings = new DeviceSettings10
+            {
+                AdapterOrdinal = 0,
+                CreationFlags = DeviceCreationFlags.None,
+                Width = WindowWidth,
+                Height = WindowHeight
+            };
+
+            InitializeDevice(settings);
+        }
+
+        protected override void OnResourceLoad()
+        {
+            using (Texture2D texture = Texture2D.FromSwapChain<Texture2D>(Context10.SwapChain, 0))
+            {
+                renderTargetView = new RenderTargetView(Context10.Device, texture);
+            }
+
+            CreateDepthBuffer();
+            var dssd = new DepthStencilStateDescription
+            {
+                IsDepthEnabled = true,
+                IsStencilEnabled = false,
+                DepthWriteMask = DepthWriteMask.All,
+                DepthComparison = Comparison.Less
+            };
+            depthStencilState = DepthStencilState.FromDescription(Context10.Device, dssd);
+
+            cube = new SimpleCube(Context10.Device, Asset.File("DefaultCamera.fx"), null);
+            plane = new SimplePlane(Context10.Device, Asset.File("DefaultCamera.fx"), null);
+            straw = new SimpleGrass(Context10.Device, Asset.File("DefaultCamera.fx"), null);
+        }
+
+        protected override void OnResourceUnload()
+        {
+            renderTargetView.Dispose();
+            depthStencilView.Dispose();
+            depthStencilState.Dispose();
+        }
+
+        protected void processInput()
+        {
+            KeyboardState keyState = m_input.ReadKeyboard();
+            MouseState mouseState = m_input.ReadMouse();
+
+            if (keyState != null)
+            {
+                float Move = 64.0f;
+
+                foreach (Key key in keyState.PressedKeys)
+                {
+                    switch (key)
+                    {
+                        case (Key.P):
+                            m_soundManager.playSingle("assets/music/10 ft. Ganja Plant - Set Me Free.wav");
+                            break;
+                        case (Key.W):
+                            m_camera.AddToCamera(0f, 0f, FrameDelta * Move, out m_proj, out m_view);
+                            break;
+                        case (Key.S):
+                            m_camera.AddToCamera(0f, 0f, -FrameDelta * Move, out m_proj, out m_view);
+                            break;
+                        case (Key.A):
+                            m_camera.AddToCamera(FrameDelta * Move, 0f, 0f, out m_proj, out m_view);
+                            break;
+                        case (Key.D):
+                            m_camera.AddToCamera(-FrameDelta * Move, 0f, 0f, out m_proj, out m_view);
+                            break;
+                        case (Key.Space):
+                            m_camera.AddToCamera(0f, FrameDelta * Move, 0f, out m_proj, out m_view);
+                            break;
+                        case (Key.Escape):
+                            m_isFormClosed = true;
+                            Quit();
+                            break;
+                        case (Key.LeftAlt | Key.Return):
+                            OnResourceUnload();
+                            isFullScreen = !isFullScreen;
+
+                            if (Context9 != null)
+                            {
+                                userInterfaceRenderer.Dispose();
+
+                                Context9.PresentParameters.BackBufferWidth = m_configuration.WindowWidth;
+                                Context9.PresentParameters.BackBufferHeight = m_configuration.WindowHeight;
+                                Context9.PresentParameters.Windowed = !isFullScreen;
+
+                                if (!isFullScreen)
+                                    m_form.MaximizeBox = true;
+
+                                Context9.Device.Reset(Context9.PresentParameters);
+
+                                userInterfaceRenderer = new UserInterfaceRenderer9(Context9.Device, m_form.ClientSize.Width, m_form.ClientSize.Height);
+                            }
+                            else if (Context10 != null)
+                            {
+                                userInterfaceRenderer.Dispose();
+
+                                Context10.SwapChain.ResizeBuffers(1, WindowWidth, WindowHeight, Context10.SwapChain.Description.ModeDescription.Format, SwapChainFlags.AllowModeSwitch);
+                                Context10.SwapChain.SetFullScreenState(isFullScreen, null);
+
+                                userInterfaceRenderer = new UserInterfaceRenderer10(Context10.Device, WindowWidth, WindowHeight);
+                            }
+
+                            OnResourceLoad();
+                            break;
+                    }
+                }
+            }
+            if (mouseState != null)
+            {
+                m_camera.RotateAroundPosition(FrameDelta * mouseState.X, -FrameDelta * mouseState.Y, out m_proj, out m_view);
+            }
+        }
+
+        protected override void OnRenderBegin()
+        {
+            Context10.Device.OutputMerger.DepthStencilState = depthStencilState;
+            Context10.Device.OutputMerger.SetTargets(depthStencilView, renderTargetView);
+
+            Context10.Device.Rasterizer.SetViewports(new Viewport(0, 0, WindowWidth, WindowHeight, 0.0f, 1.0f));
+            Context10.Device.ClearRenderTargetView(renderTargetView, new Color4(0.3f, 0.3f, 0.3f));
+            Context10.Device.ClearDepthStencilView(depthStencilView, DepthStencilClearFlags.Depth, 1.0f, 0);
+
+            processInput();
+
+            m_camera.Update(out m_proj, out m_view);
+        }
+
+        protected override void OnRender()
+        {
+            double a = clock.Check();
+            output.Value = 5*(float)System.Math.Sin(a);
+            //m_camera.Location = new Vector3(5 * (float)System.Math.Sin(a), 3 * (float)System.Math.Sin(a) + 4, 5 * (float)System.Math.Cos(a)); //Orbit around the target.
+            //camera.Location = new Vector3(0.0f, 3.0f, 4.0f);
+            //camera.MoveLeft(0.8f);
+
+            Matrix world = Matrix.Identity;
+            cube.Effect.GetVariableByName("world").AsMatrix().SetMatrix(world);
+            cube.Effect.GetVariableByName("view").AsMatrix().SetMatrix(m_view);
+            cube.Effect.GetVariableByName("proj").AsMatrix().SetMatrix(m_proj);
+            cube.Draw();
+
+            Matrix.Translation(2.0f, 0.0f, 0.0f, out world);
+            cube.Effect.GetVariableByName("world").AsMatrix().SetMatrix(world);
+            cube.Draw();
+
+            world = Matrix.Identity;
+            Matrix.Translation(0.0f, -0.5f, 0.0f, out world);
+            Matrix tempMatrix;
+            Matrix.Scaling(5.0f, 5.0f, 5.0f, out tempMatrix);
+            Matrix.Multiply(ref tempMatrix, ref world, out world);
+            plane.Effect.GetVariableByName("world").AsMatrix().SetMatrix(world);
+            plane.Effect.GetVariableByName("view").AsMatrix().SetMatrix(m_view);
+            plane.Effect.GetVariableByName("proj").AsMatrix().SetMatrix(m_proj);
+            plane.Draw();
+
+            for (int col = -50; col < 50; ++col)
+            {
+                for (int row = -50; row < 50; ++row)
+                {
+                    world = Matrix.Identity;
+                    Matrix.Scaling(0.01f, 0.1f+(float)strawSize.GetValue(col+50, row+50), 0.01f, out world);
+                    Matrix.Translation(row*10, -5.0f, col*10, out tempMatrix);
+                    Matrix temp2;
+                    Matrix.RotationY(col+row, out temp2);
+                    Matrix.Multiply(ref tempMatrix, ref world, out world);
+                    Matrix.Multiply(ref temp2, ref world, out world);
+                    straw.Effect.GetVariableByName("world").AsMatrix().SetMatrix(world);
+                    straw.Effect.GetVariableByName("view").AsMatrix().SetMatrix(m_view);
+                    straw.Effect.GetVariableByName("proj").AsMatrix().SetMatrix(m_proj);
+                    straw.Draw();
+                }
+            }
+        }
+
+        protected override void OnRenderEnd()
+        {
+            Context10.SwapChain.Present(0, SlimDX.DXGI.PresentFlags.None);
+        }
+
+        private void CreateDepthBuffer()
+        {
+            var depthBufferDesc = new Texture2DDescription
+            {
+                ArraySize = 1,
+                BindFlags = BindFlags.DepthStencil,
+                CpuAccessFlags = CpuAccessFlags.None,
+                Format = Format.D32_Float,
+                Height = WindowHeight,
+                Width = WindowWidth,
+                MipLevels = 1,
+                OptionFlags = ResourceOptionFlags.None,
+                SampleDescription = new SampleDescription(1, 0),
+                Usage = ResourceUsage.Default
+            };
+
+            using (var depthBuffer = new Texture2D(Context10.Device, depthBufferDesc))
+            {
+                depthStencilView = new DepthStencilView(Context10.Device, depthBuffer);
+            }
+        }
+    }
+}
