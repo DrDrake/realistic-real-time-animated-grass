a65ffb654c6425327031333d61929b16b8ce17dd
kaschubert <kaschubert@kaschubert-PC.fritz.box>
2011-10-27 20:34:09 +0200

    ...

---
+++ Uncut/src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uncut
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (SlimSceneController sceneController = new TestViewController()) //SampleTriangleController, SampleJupiterController
+            {
+                sceneController.Run();
+            }
+        }
+    }
+}
