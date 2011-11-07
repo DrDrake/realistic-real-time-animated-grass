36e613384f37665fa7493b2e7b215663ad267826
Peter_2 <Peter_2@peter-PC.Speedport_W_303V_Typ_A>
2011-11-07 19:36:38 +0100

    alles neu (Test)

---
+++ Uncut/src/Foundation/UI/Binding/Bindable.cs
@@ -0,0 +1,58 @@
+﻿namespace Uncut.UI.Binding
+{
+    /// <summary>
+    /// A wrapper for value types that enable their use in UI element bindings.
+    /// </summary>
+    /// <typeparam name="T">The underlying value type.</typeparam>
+    public class Bindable<T> : IBindable where T : struct
+    {
+        #region Public Interface
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        public T Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                if (!object.Equals(this.value, value))
+                {
+                    this.value = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Bindable&lt;T&gt;"/> class.
+        /// </summary>
+        public Bindable()
+            : this(default(T))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Bindable&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public Bindable(T value)
+        {
+            this.value = value;
+        }
+
+        #endregion
+        #region Implementation Detail
+
+        T value;
+
+        object IBindable.GetValue()
+        {
+            return Value;
+        }
+
+        #endregion
+    }
+}
---
+++ Uncut/src/Foundation/UI/UserInterface.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Uncut.UI
+{
+    /// <summary>
+    /// Encapsulates logical user interface state.
+    /// </summary>
+    public class UserInterface
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserInterface"/> class.
+        /// </summary>
+        public UserInterface()
+        {
+            Container = new ElementContainer();
+        }
+
+        /// <summary>
+        /// Gets or sets the interface's element container.
+        /// </summary>
+        public ElementContainer Container
+        {
+            get;
+            set;
+        }
+    }
+}
