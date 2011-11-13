f818866080bf3fea6245bcbf7b7d2f45bf11c7ce
Peter_2 <Peter_2@peter-PC.Speedport_W_303V_Typ_A>
2011-11-13 15:29:49 +0100

    Iheigthmap

--- Uncut/src/Entities/Hoehenkarte.cs
+++ Uncut/src/Entities/Hoehenkarte.cs
@@ -10,94 +10,75 @@ using Device = SlimDX.Direct3D10.Device;
 using PrimitiveTopology = SlimDX.Direct3D10.PrimitiveTopology;
 using MapFlags = SlimDX.Direct3D10.MapFlags;
 
+using Uncut.Utility;
 using Uncut.Rendering.Mesh;
 
 namespace Uncut{
 
-    class Hoehenkarte : IDisposable{
+    class Hoehenkarte : Entity{
 
-        public Effect Effect { get { return effect; } }
-
-        private Effect effect;
-        private InputLayout layout;
-        private VertexBufferBinding[] binding;
-        private InputElement[] elements;
-        private Device device;
-        private Buffer vertexBuffer;
         private Bitmap hoehenkarte;
 
-        public Hoehenkarte(Device device, string effectName)
-        {
-
-            this.device = device;
-            effect = Effect.FromFile(device, effectName, "fx_4_0");
-
-            elements = new[] {
-                new InputElement("POSITION", 0, Format.R32G32B32_Float, 0, 0),
-                new InputElement("COLOR", 0, SlimDX.DXGI.Format.R32G32B32A32_Float, 16, 0),
-                
-            };
-
-            layout = new InputLayout(device, effect.GetTechniqueByIndex(0).GetPassByIndex(0).Description.Signature, elements);
-
-            vertexBuffer = new Buffer(device,4000000 *32,
-                    ResourceUsage.Dynamic,
-                    BindFlags.VertexBuffer,
-                    CpuAccessFlags.Write,
-                    ResourceOptionFlags.None
-            );
-
-            LoadVertices();
-
-            binding = new[] { new VertexBufferBinding(vertexBuffer, 32, 0) };
-                                                                                                                                            
+        public Hoehenkarte(){
+ 
         }
 
-        private void LoadVertices(){
-            
-            hoehenkarte = new Bitmap("Resources/hoehenkarte/huegel1000.jpg");
+        public override void CreateVertexBuffer(){
+
             Color i;
             float xf = 0;
-            float yf = 0;
-            float start = 10.0f;
+            float zf = 0;
+            float start = 0;
+            hoehenkarte = new Bitmap("Resources/texture/heightmap/huegel1000x1000.jpg");
             
-            DataStream stream = vertexBuffer.Map(MapMode.WriteDiscard, MapFlags.None);
-
+            m_numberOfElements = 1000000;
+            m_vertexBuffer = InitVertexBuffer();
+            SVertex3P3N2T[] vertices = new SVertex3P3N2T[m_numberOfElements];
+                for (int y = 0; y < 1000; y++){
+                    zf = y * 0.5f;
                     for (int x = 0; x < 1000; x++){
-                        xf = x * 0.1f;
-                        for (int y = 0; y < 1000; y++){
-
-                            yf = y * 0.1f;
-                            i = hoehenkarte.GetPixel(x, y);
-                            //System.Console.Write("Wert " + (i.GetBrightness() * 100) + ", "); 
-                    stream.WriteRange(new[] {
-                //          Position                              Farbe205,170,125
-
-                new Vector4(start + 0.1f +  xf, 0.0f + (i.GetBrightness()*10), start + 0.1f + yf, 1.0f), new Vector4(0.8f, 0.66f, 0.5f, 1.0f),//or
-                new Vector4(start +         xf, 0.0f + (i.GetBrightness()*10), start + 0.1f + yf, 1.0f), new Vector4(0.8f, 0.66f, 0.5f, 1.0f),//ur
-				new Vector4(start + 0.1f +  xf, 0.0f + (i.GetBrightness()*10), start        + yf, 1.0f), new Vector4(0.8f, 0.66f, 0.5f, 1.0f),//lo
-                new Vector4(start +         xf, 0.0f + (i.GetBrightness()*10), start        + yf, 1.0f), new Vector4(0.8f, 0.66f, 0.5f, 1.0f),//lu
-			});
-                     }
+                        xf = x * 0.5f;
+                        i = hoehenkarte.GetPixel(x, y);
+                        vertices[(y * 1000) + x] = new SVertex3P3N2T(new Vector3(start + xf, 0.0f + ((i.GetBrightness() * 10) - 10), start + zf), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(1.0f, 1.0f));
+                    }
                 }
-                    
-            vertexBuffer.Unmap();
+
+            DataStream stream = m_vertexBuffer.Map(MapMode.WriteDiscard, MapFlags.None);
+            stream.WriteRange<SVertex3P3N2T>(vertices);
+            m_vertexBuffer.Unmap();
+                                                                                                                           
         }
 
-        public void Draw(){
+        public override void CreateIndexBuffer(){
 
-            device.InputAssembler.SetInputLayout(layout);
-            device.InputAssembler.SetPrimitiveTopology(PrimitiveTopology.LineStrip);// Strip benötigt nur vier Punkte.Punkte der diagonale werden nur einmal gebraucht
-            device.InputAssembler.SetVertexBuffers(0, binding);
-            effect.GetTechniqueByIndex(0).GetPassByIndex(0).Apply();
-            device.Draw(4000000, 0);
+            m_indexCount = 998001 * 6;
+            m_indexBuffer = InitIndexBuffer();
+            UInt32[] indices = new UInt32[m_indexCount];
 
+            int z = 0;
+            for (UInt32 y = 0; y < 999; y++){
+
+                for (UInt32 x = 0; x < 999; x++){
+
+                    indices[z] = y * 1000 + x;
+                    z++;
+                    indices[z] = y * 1000 + x + 1001;
+                    z++;
+                    indices[z] = y * 1000 + x + 1;
+                    z++;
+                    indices[z] = y * 1000 + x;
+                    z++;
+                    indices[z] = (y + 1) * 1000 + x;
+                    z++;
+                    indices[z] = y * 1000 + x + 1001;
+                    z++;
+                    }
+                }
+
+                DataStream stream = m_indexBuffer.Map(MapMode.WriteDiscard, MapFlags.None);
+                stream.WriteRange<UInt32>(indices);
+                m_indexBuffer.Unmap();
+
+            }
         }
-
-        public void Dispose(){
-
-            effect.Dispose();
-            layout.Dispose();
-        }  
-    }
 }
