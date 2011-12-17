b7e86ad527e4e15665884d93199d9cc66786d189
Sandra <Sandra@Black.congstar.box>
2011-12-17 20:36:56 +0100

    Lala

---
+++ RealtimeGrass/src/Entities/Light.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+using SlimDX;
+using SlimDX.Direct3D10;
+using SlimDX.DXGI;
+using SlimDX.D3DCompiler;
+using Buffer = SlimDX.Direct3D10.Buffer;
+using Device = SlimDX.Direct3D10.Device;
+using PrimitiveTopology = SlimDX.Direct3D10.PrimitiveTopology;
+using MapFlags = SlimDX.Direct3D10.MapFlags;
+
+using RealtimeGrass.Rendering.Mesh;
+using RealtimeGrass.Utility;
+
+namespace RealtimeGrass.Entities
+{
+    class Light
+    {
+        private Vector3 m_color;
+        public Vector3 Color { get { return m_color; } set { m_color = value; } }
+        private Vector3 m_direction;
+        public Vector3 Direction { get { return m_direction; } set { m_direction = value; } }
+
+        public Light(Vector3 color, Vector3 dir)
+        {
+            m_color = color;
+            m_direction = dir;
+        }
+    }
+}
---
+++ RealtimeGrass/src/Foundation/UI/ElementContainer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Collections;
+
+namespace RealtimeGrass.UI
+{
+    /// <summary>
+    /// A container for UI elements.
+    /// </summary>
+    public class ElementContainer : IEnumerable<Element>
+    {
+        #region Public Interface
+
+        /// <summary>
+        /// Adds the specified element to the container.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        public void Add(Element element)
+        {
+            elements.Add(element);
+        }
+
+        public void Update()
+        {
+            foreach (Element element in elements)
+            {
+                element.Update();
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator"/> object that can be used to iterate through the collection.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Collections.Generic.IEnumerator`1"/> that can be used to iterate through the collection.
+        /// </returns>
+        IEnumerator<Element> IEnumerable<Element>.GetEnumerator()
+        {
+            foreach (Element element in elements)
+                yield return element;
+        }
+
+        #endregion
+        #region Implementation Detail
+
+        List<Element> elements = new List<Element>();
+
+        #endregion
+    }
+}
